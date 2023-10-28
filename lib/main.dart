38fc8f5f484a09f830d068bdabb4dadbb20c1722
MUHAMMEDMINHAJP <minhajpulikkaparambil@gmail.com>
2023-10-28 15:42:44 +0530

    initialcommit

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MaterialApp(home: Center(child: Text("Hello",style: TextStyle(color: Colors.white,fontStyle: FontStyle.italic, decoration: TextDecoration.none))),));
+
+}
